f8e2a0b078d20e1940cd2d7eb4f577a8a1ff1ab9
ajuchem <ajuchem@daudt.com>
2016-03-20 20:49:09 -0600

    Make the JSON request

--- AJAX-and-APIs/js/flickr.js
+++ AJAX-and-APIs/js/flickr.js
@@ -2,5 +2,15 @@ $(document).ready(function(){
   $("button").click(function(){
     $("button").removeClass("selected");
     $(this).addClass("selected");
+    var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+    var animal = $(this).text();
+    var flickrOptions = {
+      tags: animal,
+      format: "json"
+    } // end flickrOptions
+    function displayPhotos(data){
+
+    } // end displayPhotos
+    $.getJSON(flickerAPI, flickrOptions, displayPhotos);
   }); // end click
 }); // end ready
